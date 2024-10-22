80a6c7661d1e7a703758e40aee7fe666f1738a8d
Jenna Rigby <RGBJEN001@myuct.ac.za>
2024-10-22 21:58:58 +0200

    Added methods to retrieve and recommend books by genre.

--- src/Chatbot.py
+++ src/Chatbot.py
@@ -1,1 +1,21 @@
-from src.Recommendations import recommendations
+from src.Recommendations import Recommendations
+
+class Chatbot:
+    def __init__(this):
+        Recommendations = Recommendations()
+
+    def begin(this):
+        print("Welcome to the book recommendation chatbot! ")
+
+        while True:
+            genre = input("What genre of books would you like to read? (enter 'exit' to quit) ")
+
+            if (genre == 'exit' or genre == 'EXIT'):
+                print("Thanks for participating! ")
+
+
+
+
+if __name__ == "__main__":
+    chatbot = Chatbot()
+    chatbot.begin()
--- src/Recommendations.py
+++ src/Recommendations.py
@@ -1,1 +1,8 @@
+from src.Google_books import Google_books
 
+class Recommendations:
+    def __init__(self):
+        self.Google_books = Google_books
+
+    def recommend_genre(self, genre):
+        return self.Google_books.recommend_genre(genre)
